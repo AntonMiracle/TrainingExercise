bef998044c0723641e7ec62809e820abecc8ca54
AntonMiracle <b.anton.m.1986@gmail.com>
2016-05-31 09:01:18 +0200

    Commit

---
+++ src/com/bam/javaee/Runner.java
@@ -0,0 +1,23 @@
+/** 
+ * Project: TrainingExercise
+ * Runner.java  
+ */
+package com.bam.javaee;
+
+
+/**
+ * @author Bondarenko Anton Michailovich
+ * @version 1.0
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
